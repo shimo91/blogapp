84807d898a6b7036cc0f98b5352d65067e43cc37
Shimona <141317393+shimo91@users.noreply.github.com>
2023-11-19 15:47:35 +0400

    Update Login.jsx

--- src/ui-components/Login.jsx
+++ src/ui-components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     }
     const addHandler=()=>{
         console.log(user);
-        axios.post('http://127.0.0.1:4000/user/login',user).then((res)=>{
+        axios.post('http://54.159.167.39:4000/user/login',user).then((res)=>{
           alert(res.data.message);
       if(res.data.message=='success')
       {
